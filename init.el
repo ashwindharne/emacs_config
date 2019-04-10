6e64780b5632e7561e0ee3787f018c43264db2dd
Ashwin Dharne <ashwindharne@dm.ai>
2019-04-09 18:21:13 -0700

    added markdown support

--- init.el
+++ init.el
@@ -64,3 +64,11 @@
 (setq yas-triggers-in-field t)
 (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 
+;;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "pandoc"))
